3ed56d0842d109d5a54b7fb6a27885217ef7e7c8
Bruno Dezorzi <brunodezorzi9@gmail.com>
2025-02-27 22:23:32 -0300

    27/02/2025

--- 4 Semestre/Banco de Dados II/exercicio_1_functions.sql
+++ 4 Semestre/Banco de Dados II/exercicio_1_functions.sql
@@ -1,7 +1,9 @@
+-- 1
+
 DROP TABLE  IF EXISTS  pagamentos;
+DROP TABLE  IF EXISTS  pedidos;
 DROP TABLE  IF EXISTS  produtos;
 DROP TABLE  IF EXISTS  fornecedor;
-DROP TABLE  IF EXISTS  pedidos;
 DROP TABLE  IF EXISTS  clientes;
 
 
@@ -12,15 +14,6 @@ CREATE TABLE clientes (
 	nome varchar(225), 
 	email varchar(225), 
 	telefone varchar(225)
-);
-
-
-CREATE  TABLE  pedidos (
-	id_pedido serial PRIMARY KEY ,
-	id_cliente int,
-	data_pedido date,
-	status varchar(225),
-	FOREIGN KEY (id_cliente) REFERENCES clientes(id_cliente)
 );
 
 CREATE  TABLE  fornecedor(
@@ -37,6 +30,21 @@ CREATE  TABLE  produtos(
 	FOREIGN KEY (id_fornecedor) REFERENCES fornecedor(id_fornecedor)
 );
 
+
+
+CREATE  TABLE  pedidos (
+	id_pedido serial PRIMARY KEY ,
+	id_cliente int,
+	id_produto int,
+	data_pedido date,
+	status varchar(225),
+	FOREIGN KEY (id_cliente) REFERENCES clientes(id_cliente),
+	FOREIGN KEY (id_produto) REFERENCES produtos(id_produto)
+);
+
+
+
+
 CREATE  TABLE  pagamentos(
 	id_pagamento serial PRIMARY KEY,
 	id_pedido int,
@@ -45,6 +53,9 @@ CREATE  TABLE  pagamentos(
 	data_pagamento date,
 	FOREIGN KEY (id_pedido) REFERENCES pedidos(id_pedido)
 );
+
+
+--2
 
 INSERT INTO clientes(
 	nome , 
@@ -59,19 +70,6 @@ VALUES
 ('Antonio','Antonio@brasil.com','4574312549' );
 
 
-INSERT INTO pedidos(
-	id_cliente ,
-	data_pedido ,
-	status 
-)
-VALUES
-(1,'2025-05-02','indo' ),
-(2,'2025-06-09','voltando' ),
-(3,'2025-07-06','pensando em ir' ),
-(4,'2025-04-03','esperando dar vontade' ),
-(5,'2025-03-01','descansado' );
-
-
 INSERT INTO fornecedor(
 	nome ,
 	contato 
@@ -82,7 +80,6 @@ VALUES
 ('Bilu','45130468795' ),
 ('Clair','45467898132' ),
 ('Chris','4546878421' );
-
 
 INSERT INTO produtos(
 	nome,
@@ -95,6 +92,25 @@ VALUES
 ('Caderno',15.00,3),
 ('Régua',5.00,4),
 ('Apontador',4.00,5);
+
+
+INSERT INTO pedidos(
+	id_cliente ,
+	id_produto,
+	data_pedido ,
+	status 
+)
+VALUES
+(1,5,'2025-05-02','indo' ),
+(2,4,'2025-06-09','voltando' ),
+(3,3,'2025-07-06','pensando em ir' ),
+(4,2,'2025-04-03','esperando dar vontade' ),
+(5,1,'2025-03-01','descansado'),
+(5,1,'2025-05-02','indo'),
+(4,2,'2025-06-09','voltando' ),
+(3,3,'2025-07-06','pensando em ir' ),
+(2,4,'2025-04-03','esperando dar vontade' ),
+(1,5,'2025-03-01','descansado' );
 
 	
 	
@@ -112,6 +128,7 @@ VALUES
 (1,20.00,'C','2025-03-08');
 
 
+--3
 
 CREATE OR REPLACE 
 FUNCTION insert_cliente(nome varchar(225),email varchar(225),telefone varchar(225)) 
@@ -141,6 +158,8 @@ SELECT insert_cliente('Claudio','Claudio@brasil.com','45987132465')
 
 DROP FUNCTION update_preco_produto
 
+--4
+
 CREATE OR REPLACE FUNCTION update_preco_produto(id_produto_modificar INT, novo_preco NUMERIC)
 RETURNS VOID 
 AS
@@ -158,4 +177,83 @@ LANGUAGE PLPGSQL;
 SELECT update_preco_produto(1, 300.0);
 
 
-SELECT * FROM produtos
+SELECT * FROM produtos;
+
+SELECT * FROM pedidos;
+
+--5
+
+DROP FUNCTION buscar_pedidos_cliente
+
+CREATE OR REPLACE FUNCTION buscar_pedidos_cliente(id_cliente_analise int)
+RETURNS TABLE (id_pedido int, id_cliente int, data_pedido date, status varchar(225)) 
+AS
+$$
+BEGIN
+	RETURN QUERY 
+	SELECT pedidos.id_pedido, pedidos.id_cliente, pedidos.data_pedido, pedidos.status 
+	FROM pedidos 
+	WHERE pedidos.id_cliente = id_cliente_analise;
+END;
+$$
+LANGUAGE plpgsql;
+
+
+SELECT * FROM buscar_pedidos_cliente(2);
+
+--6
+
+DROP FUNCTION calcular_total_pedido(integer)
+
+CREATE OR REPLACE FUNCTION calcular_total_pedido(id_pedido_ int)
+RETURNS TABLE (soma numeric)
+AS 
+$$
+	
+	begin
+		return query
+		select
+			sum(produtos.preco) as soma
+		from pedidos
+		inner join produtos on pedidos.id_produto = produtos.id_produto 
+		where pedidos.id_pedido = id_pedido_;
+	end ;
+$$
+LANGUAGE plpgsql;
+
+SELECT calcular_total_pedido(1)
+
+--7
+
+DROP FUNCTION IF EXISTS remover_pedido(integer);
+DROP FUNCTION IF EXISTS null_produto_pedido(integer);
+
+
+CREATE OR REPLACE FUNCTION null_produto_pedido(id_pedido_ int)
+RETURNS void 
+AS
+$$
+	begin
+		update pedidos
+		set id_produto = null
+		where pedidos.id_pedido = id_pedido_;
+	end;
+
+$$
+LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION remover_pedido(id_pedido_ int)
+RETURNS void 
+AS
+$$
+	begin
+		perform null_produto_pedido(id_pedido_);
+		delete from pedidos
+		where pedidos.id_pedido = id_pedido_;
+	end;
+$$
+LANGUAGE plpgsql;
+
+-- SELECT * FROM pedidos
+
+SELECT remover_pedido(1)
